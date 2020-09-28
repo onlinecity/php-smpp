37a17a18fe96d539deb411ff161edcf4ab8a8860
Alexandr Mironov <mironov.a.s@outlook.com>
2020-09-28 13:41:49 +0400

    update Smpp Address

--- src/Address.php
+++ src/Address.php
@@ -19,13 +19,21 @@ class Address
      * @param string $value
      * @param integer $ton
      * @param integer $npi
-     * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
      */
-    public function __construct($value,$ton=SMPP::TON_UNKNOWN,$npi=SMPP::NPI_UNKNOWN)
+    public function __construct(
+        $value,
+        $ton = SMPP::TON_UNKNOWN,
+        $npi = SMPP::NPI_UNKNOWN
+    )
     {
         // Address-Value field may contain 10 octets (12-length-type), see 3GPP TS 23.040 v 9.3.0 - section 9.1.2.5 page 46.
-        if ($ton == SMPP::TON_ALPHANUMERIC && strlen($value) > 11) throw new InvalidArgumentException('Alphanumeric address may only contain 11 chars');
-        if ($ton == SMPP::TON_INTERNATIONAL && $npi == SMPP::NPI_E164 && strlen($value) > 15) throw new InvalidArgumentException('E164 address may only contain 15 digits');
+        if ($ton == SMPP::TON_ALPHANUMERIC && strlen($value) > 11) {
+            throw new \InvalidArgumentException('Alphanumeric address may only contain 11 chars');
+        }
+        if ($ton == SMPP::TON_INTERNATIONAL && $npi == SMPP::NPI_E164 && strlen($value) > 15) {
+            throw new \InvalidArgumentException('E164 address may only contain 15 digits');
+        }
 
         $this->value = (string) $value;
         $this->ton = $ton;
